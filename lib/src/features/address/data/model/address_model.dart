f31f90c49b67e9027493fe3af343ae3316ac371c
SubramanyaKS <subramanyaks2205@gmail.com>
2025-06-04 06:41:48 +0530

    inckuded the models for nammkart

---
+++ lib/src/features/address/data/model/address_model.dart
@@ -0,0 +1,27 @@
+import 'package:nammkart/src/features/address/domain/entities/address.dart';
+
+class AddressModel extends AddressEntity {
+  AddressModel({required super.userId, super.city,super.street,required super.postalcode,required super.state,required super.country});
+
+factory AddressModel.fromJson(Map<String, dynamic> json) {
+    return AddressModel(
+      userId: json['userId'],
+      street: json['street'],
+      city: json['city'],
+      state: json['state'],
+      postalcode: json['postalcode'],
+      country: json['country']
+    );
+}
+
+  Map<String, dynamic> toJson() {
+    return {
+      'userId': userId,
+      'street': street,
+      'city': city,
+      'state': state,
+      'postalcode': postalcode,
+      'country': country,
+    };
+  }
+}
